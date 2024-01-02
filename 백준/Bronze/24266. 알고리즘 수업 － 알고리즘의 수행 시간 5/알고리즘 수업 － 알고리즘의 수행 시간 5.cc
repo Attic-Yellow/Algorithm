249ddaaf4ae96a91f285716fdfd3183df22d6ebf
DongHee Lee <117146643+Attic-Yellow@users.noreply.github.com>
2024-01-02 22:42:12 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/24266. 알고리즘 수업 － 알고리즘의 수행 시간 5/알고리즘 수업 － 알고리즘의 수행 시간 5.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	long long N; 
+    cin >> N;
+    
+	cout << N * N * N << '\n' << 3;
+    
+    return 0;
+}
