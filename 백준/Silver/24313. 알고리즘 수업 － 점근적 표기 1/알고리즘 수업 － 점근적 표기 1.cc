10847a5b2a8784b1e16d9f15dff98dde7d26f2c2
DongHee Lee <117146643+Attic-Yellow@users.noreply.github.com>
2024-01-04 23:14:57 +0900

    [Silver V] Title: 알고리즘 수업 - 점근적 표기 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/24313. 알고리즘 수업 － 점근적 표기 1/알고리즘 수업 － 점근적 표기 1.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int a1, a0, c, n0; 
+    cin >> a1 >> a0 >> c >> n0;
+    
+	cout << ((a1 * n0 + a0 <= c * n0) && (c - a1 >= 0));
+    
+    return 0;
+}
